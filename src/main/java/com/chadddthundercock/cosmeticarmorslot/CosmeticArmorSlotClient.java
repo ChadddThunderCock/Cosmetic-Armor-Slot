892aa659e520fd1866926d329ec42960005330dd
lost.exe <145328344+ChadddThunderCock@users.noreply.github.com>
2025-09-04 18:21:32 +0200

    initial

---
+++ src/main/java/com/chadddthundercock/cosmeticarmorslot/CosmeticArmorSlotClient.java
@@ -0,0 +1,4 @@
+package com.chadddthundercock.cosmeticarmorslot;
+
+public class CosmeticArmorSlotClient {
+}
---
+++ src/main/java/com/chadddthundercock/cosmeticarmorslot/CosmeticArmorSlotDataGenerator.java
@@ -0,0 +1,11 @@
+package com.chadddthundercock.cosmeticarmorslot;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CosmeticArmorSlotDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
